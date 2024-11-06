5aa4d051e71b900da50421407964347c4f02f6f4
Angkasa <gilangkasabintang@gmail.com>
2024-11-06 21:18:07 +0700

    TERNEWWW

---
+++ Desain Web/My-Project/Perulangan-2.php
@@ -0,0 +1,7 @@
+<?php
+$jumlah = 0;
+for ($a = 2; $a <= 50;  $a++){
+    $jumlah += $a;
+    echo "Jumlah Perulangan Penambahan 2 sampai 50 : ".$jumlah. "<br>";
+}
+?>
---
+++ Desain Web/My-Project/Perulangan-3.php
@@ -0,0 +1,7 @@
+<?php
+for ($i = 3; $i <=127; $i++){
+    if ($i % 6 == 0){
+        echo "Hasil Bilangan Bulat dari 3 sampai 127 kelipatan 6 : ".$i."<br>";
+    }
+}
+?>
